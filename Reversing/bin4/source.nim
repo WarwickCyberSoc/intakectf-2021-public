48ff7d258bb82ce7168a5ad210057d8b174993e4
joe-butler2 <72922017+joe-butler2@users.noreply.github.com>
2021-10-26 21:20:18 +0100

    Update source.nim

--- Reversing/bin4/source.nim
+++ Reversing/bin4/source.nim
@@ -1,8 +1,4 @@
-#nim c -d:release source.nim
-#key="secret_key_no_one_will_ever_guess" (can be found in gimmeKey function)
-#to get WMG{x0r1ng_d035n't_3ncrypT_s7uff} from
-#https://gchq.github.io/CyberChef/#recipe=From_Base64('A-Za-z0-9%2B/%3D',false)XOR(%7B'option':'Latin1','string':'rtwyjtagresrhrtykevddddddtrhndw3f'%7D,'Standard',false)&input=QVJFVUN3OEFQZ3dYSEN3Y0J5MGJGdzQ2QVEwSUNEc0JFaEVBTndrUkVrQVY
-#https://gchq.github.io/CyberChef/#recipe=Find_/_Replace(%7B'option':'Regex','string':'h%7C%20'%7D,'',true,false,true,false)From_Hex('Comma')XOR(%7B'option':'Latin1','string':'secret_key_no_one_will_ever_guess'%7D,'Standard',false)&input=MjRoLCAyOGgsIDI0aCwgMDksIDFEaCwgNDRoLCAyRGgsIDVBaCwgMEJoLCAxRWgsIDAwLCAwQWgsNUZoLCA2Q2gsIDVBaCwgMDAsIDQyaCwgMkJoLCAyOGgsIDVBaCwgMDIsIDBGaCwgMkRoLCAxQ2gsMDYsIDMxaCwgMkRoLCAyQ2gsIDUwaCwgMDAsIDAzLCAxNWgsIDBFaCwgMEFo
+#comments from other solution
 import strformat
 import base64
 
